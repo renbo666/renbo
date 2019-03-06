627fe3fa851c4e65b6527b4b69c3906d465872aa
bobo <18189270679@163.com>
2019-03-06 20:11:15 +0800

    tijiao lo

---
+++ test/src/com/bobo/git/FirstClass.java
@@ -0,0 +1,16 @@
+package com.bobo.git;
+
+/**
+ * @author renbo
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+		int a =10;
+    }
+}
